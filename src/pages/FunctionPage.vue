dcee42c7c548f31ce32ef9861a84ca6a9f58b42c
ohm41321 <ohm.roexe1488@gmail.com>
2023-10-06 16:36:52 +0700

    quasar-unit:test-tester

---
+++ src/pages/FunctionPage.vue
@@ -0,0 +1,53 @@
+
+<template>
+  <div class="htmlClass">
+    <h1>{{ title }}</h1>
+    <form>
+      <input type="text" id="firstValue" v-model="firstValue" />
+      <input type="text" id="secondValue" v-model="secondValue" />
+      <input type="text" id="answerValue" v-model="answerValue" />
+      <button @click="addition(firstValue,secondValue)" >Addition</button>
+      <button @click="subtraction(firstValue,secondValue)" >Subtraction</button>
+      <button @click="multiplication(firstValue,secondValue)" >Multiplication</button>
+      <button @click="divide(firstValue,secondValue)" >Divide</button>
+  </form>
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'HtmlComponent',
+  data() {
+    return {
+      title: 'Athit Fongkhaimuk',
+      firstValue:0,
+      secondValue:0,
+      answerValue:0
+    }
+  },
+  methods: {
+    // https://jestjs.io/docs/using-matchers
+    // https://www.philbarresi.com/jest-test-guide/
+    addition(first, second) {
+      this.answerValue =  Number(first) + Number(second)
+      return this.answerValue;
+    },
+    subtraction(first, second ) {
+      this.answerValue =  Number(first) - Number(second)
+      return this.answerValue;
+    },
+    multiplication(first, second ) {
+      this.answerValue =  Number(first) * Number(second)
+      return this.answerValue;
+    },
+    divide(first, second) {
+      this.answerValue =  Number(first) / Number(second)
+      return this.answerValue;
+    },
+  },
+}
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,46 @@
+<!-- <template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script> -->
+
+
+
+<!-- IndexPage.vue -->
+<template>
+  <div class="htmlClass">
+    <h1>{{ title }}</h1>
+    <InputPage/>
+    <FunctionPage/>
+  </div>
+</template>
+
+
+<script>
+import InputPage from '../pages/InputPage.vue'
+import FunctionPage from '../pages/FunctionPage.vue'
+
+export default {
+  name: 'HtmlComponent',
+  components:{InputPage, FunctionPage},
+  data() {
+    return {
+      title: 'Athit Fongkhaimuk',
+    }
+  },
+}
+</script>
+
+<style scoped></style>
